d1da2b2d9e9bebf81bbfc1e1da34a6713af16e36
KseniaFalcoz <kseniafalcoz@gmail.com>
2021-04-13 11:44:51 +0200

    ajouter bonus devinette

---
+++ js/devinette.js
@@ -0,0 +1,23 @@
+function flagСolors(){
+    let a = document.getElementById("colorNumber").value;
+    let x = 50;
+
+  switch(true) {
+    case(a > x) :
+    alert('Non, ce nombre est plus grand que prévu.');
+        break;
+    case(a < x) :
+    alert('Non, ce nombre est plus petit que prévu.')
+        break;
+    case(a == x) :
+    alert('Tu as deviné !')
+        break;
+    default: 
+    alert('Entrez un chiffre !');
+    }
+}
+function clearInput() {
+  let emptyField=""
+  document.getElementById('colorNumber').value = emptyField;
+}
+
